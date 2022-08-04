de97262b257ae6c7ffdcddb4489e537ec3ea9475
chloemyha <tup14113@temple.edu>
2022-08-04 19:13:50 -0400

    1st

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,56 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
-
+    @Test
+    public void onlyBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void twoBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+    @Test
+    public void bracketsInBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+    @Test
+    public void bracketsInBracketWithBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]] []"));
+    }
+    @Test
+    public void tripleBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
+    @Test
+    public void tripleDoubleBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[][]]]"));
+    }
+    @Test
+    public void wordsInBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+    @Test
+    public void wordsInAndOutBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+    @Test
+    public void emptyBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+    @Test
+    public void singleOpenBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void singleCloseBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void BackFacingBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void wordsInBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch]Code]"));
+    }
 }
